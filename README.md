e5a51746d621dea8bc790723d01849d6fb35f87c
fthmko <fthmko@gmail.com>
2020-08-18 14:52:56 +0800

    upload files

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # jenkins-theme
+
+Make jenkins looks better with Material Design Icon.
+
+## Features
+
+- Replace the old Tango Icon by [Material Design Icon](https://materialdesignicons.com "Material Design Icon")
+- Remove all border radius
+- Fix some vertical align(checkbox, icon, etc.)
+- Material Design button style from [afonsof/jenkins-material-theme](https://github.com/afonsof/jenkins-material-theme)
+- Syntax Highlight theme from [afonsof/jenkins-material-theme](https://github.com/afonsof/jenkins-material-theme)
+
+## Screenshots
+![](screenshots/screenshot-1.png)
+![](screenshots/screenshot-2.png)
+![](screenshots/screenshot-3.png)
+![](screenshots/screenshot-4.png)
+
+## Installation
+### Use Online Resource
+1. Install [Simple Theme Plugin](https://plugins.jenkins.io/simple-theme-plugin)
+2. Click `Manage Jenkins`
+3. Click `Configure System` and scroll down to `Theme`
+4. Add a `Javascript URL` with value `https://cdn.rawgit.com/fthmko/jenkins-theme/userContent/theme.js`
+5. Add a `CSS URL` with value `https://cdn.rawgit.com/fthmko/jenkins-theme/userContent/theme.css`
+6. Add a `CSS URL` with value `https://cdn.jsdelivr.net/npm/@mdi/font@5.5.55/css/materialdesignicons.min.css`
+7. `Save` and Refresh your browser
+
+### Use Local Resource
+1. Download this repo and upload files under `userContent` to your host's `$JENKINS_HOME/userContent`
+2. Download [Material Design Icon](https://materialdesignicons.com "Material Design Icon") (version 5.5.5) and upload files to your host's `$JENKINS_HOME/userContent` 
+3. Follow the previous method steps and change the URLs to your own
---
+++ userContent/theme.js
@@ -0,0 +1,254 @@
+(function() {
+	let MMP = {
+		'new-package.png': 'mdi-text-box-plus',
+		'new-user.png': 'mdi-account-plus',
+		'user.png': [{
+			pos: 'left',
+			href: '/asynchPeople/',
+			value: 'mdi-account-multiple'
+		}, {
+			pos: 'main',
+			value: 'mdi-account-multiple'
+		}, {
+			pos: 'left',
+			href: '/my-views',
+			value: 'mdi-folder-star'
+		}],
+		'notepad.png': [{
+			pos: 'left',
+			value: 'mdi-history'
+		}, {
+			pos: 'main',
+			value: 'mdi-script-text'
+		}],
+		'search.png': [{
+			pos: 'left',
+			href: '/projectRelationship',
+			value: 'mdi-vector-link'
+		}, {
+			pos: 'left',
+			href: '/',
+			value: 'mdi-text-box-outline'
+		}],
+		'fingerprint.png': 'mdi-fingerprint',
+		'gear2.png': 'mdi-cog',
+		'gear.png': 'mdi-folder-edit',
+		'blueocean.png': 'mdi-digital-ocean',
+		'device-24x24.png': 'mdi-table-lock',
+		'folder.png': [{
+			pos: 'left',
+			href: '/newView',
+			value: 'mdi-folder-plus'
+		}, {
+			pos: 'left',
+			href: '/ws/',
+			value: 'mdi-folder-open'
+		}, {
+			pos: 'table',
+			value: 'mdi-folder-open-outline _th_link'
+		}, {
+			pos: 'main',
+			value: 'mdi-folder-open'
+		}],
+		'folder-delete.png': 'mdi-delete-circle _th_red',
+		'up.png': 'mdi-arrow-up-thick',
+		'setting.png': 'mdi-application-cog',
+		'edit-delete.png': [{
+			pos: 'left',
+			href: '/confirmDelete',
+			value: 'mdi-text-box-remove _th_red'
+		}, {
+			pos: 'left',
+			href: 'delete',
+			value: 'mdi-folder-remove _th_red'
+		}, {
+			pos: 'left',
+			href: '#',
+			value: 'mdi-text-box-remove _th_red'
+		}],
+		'credentials.png': 'mdi-key-chain',
+		'clock.png': 'mdi-motion-play',
+		'star.png': 'mdi-star-outline',
+		'move.png': 'mdi-file-move',
+		'terminal.png': [{
+			pos: 'left',
+			value: 'mdi-clipboard-text-outline'
+		},{
+			pos: 'main',
+			value: 'mdi-powershell'
+		}],
+		'clipboard.png': 'mdi-clipboard-flow-outline',
+		'git-32x32.png': 'mdi-git',
+		'git-48x48.png': 'mdi-git',
+		'save.png': [{
+			pos: 'left',
+			href: '/tagBuild',
+			value: 'mdi-tag-multiple-outline'
+		}],
+		'previous.png': 'mdi-arrow-left-circle',
+		'next.png': [{
+			pos: 'left',
+			href: '/emailexttemplates',
+			value: 'mdi-text-box-multiple'
+		}],
+		'document.png': 'mdi-text-box-outline',
+
+		'grey_anime.gif': 'mdi-loading mdi-spin',
+		'grey.png': 'mdi-web',
+		'blue.png': 'mdi-check-circle _th_green',
+		'red.png': 'mdi-close-octagon _th_red',
+		'yellow.png': 'mdi-alert-decagram-outline _th_yellow',
+		'health-60to79.png': 'mdi-weather-partly-cloudy',
+		'health-80plus.png': 'mdi-weather-sunny',
+		'collapse.png': 'mdi-chevron-up',
+		'expand.png': 'mdi-chevron-down',
+		'atom.gif': 'mdi-rss-box',
+
+		'plugin.png': 'mdi-puzzle',
+		'network.png': 'mdi-server-network',
+		'secure.png': 'mdi-shield-account',
+		'computer.png': 'mdi-information',
+		'monitor.png': 'mdi-clipboard-pulse',
+		'help.png': 'mdi-help-box',
+		'refresh.png': 'mdi-cog-refresh',
+		'system-log-out.png': 'mdi-remote-desktop',
+		'mail-mark-read.png': 'mdi-email-multiple',
+		'orange-square.png' : 'mdi-account-circle-outline',
+		'spinner.gif': 'mdi-vanish-quarter mdi-spin',
+		'new-credential.png': 'mdi-key-plus',
+		'none.gif': 'mdi-blank',
+		'system-store.png': 'mdi-home-lock',
+		'copyright-vertical.png': 'SKIP',
+		'select-all.png': 'mdi-checkbox-multiple-marked-outline',
+		'unselect-all.png': 'mdi-checkbox-multiple-blank-outline',
+		'stop.png': 'mdi-close-box _th_red',
+		'ssh-key.png': 'mdi-file-key-outline',
+		'userpass.png': 'mdi-account-key',
+		'domain.png': 'mdi-web-box',
+	};
+
+	function init_theme($) {
+		theme_left($);
+		theme_table($);
+		theme_history($);
+		theme_rss($);
+		theme_main($);
+	}
+
+	function mapping(img, pos, href) {
+		let l1 = MMP[img];
+		if(img.includes('?')){
+			return '';
+		}
+		if (!l1) {
+			console.warn('MAP MIS1:', img, pos);
+			return '';
+		}
+
+		if (typeof(l1) == 'string') {
+			return l1;
+		}
+
+		let params = [{
+			key: 'pos',
+			value: pos || ''
+		}, {
+			key: 'href',
+			value: href || ''
+		}];
+		let rst = l1;
+		for (let prm of params) {
+			nxt = rst.filter(v => !v[prm.key] || prm.value.endsWith(v[prm.key]));
+			if (!nxt.length) {
+				console.warn(`MAP MIS: ${img} on ${prm.key}=${prm.value}`);
+				return '';
+			} else if (nxt.length == 1) {
+				return nxt[0].value;
+			}
+			rst = nxt;
+		}
+
+		console.warn('MAP MULTI:', img, pos, href);
+		return '';
+	}
+
+	function theme_left($) {
+		$('#tasks img').each(function() {
+			prc_img($(this), 'left');
+		});
+
+	}
+
+	function theme_table($) {
+		$('#projectstatus img').each(function() {
+			prc_img($(this), 'table');
+		});
+
+		$('#projectstatus .icon-fav-inactive').each(function() {
+			let img = $(this);
+			let pt = img.parent();
+
+			let icon = mapping('star.png', 'table');
+			let sz = img.height();
+
+			img.replaceWith(`<span class="mdi mdi-${sz}px ${icon}"></span>`);
+		});
+	}
+
+	function theme_history($) {
+		$('#buildHistory img').each(function() {
+			prc_img($(this), 'history');
+		});
+		$('#buildHistory').on('DOMSubtreeModified', (e) => {
+			if (e.target.tagName.toLowerCase() == 'tbody' && $('#buildHistory img').length) {
+				prc_img($($('#buildHistory img')[0]), 'history');
+			}
+		})
+
+	}
+
+	function theme_rss($) {
+		$('#rss-bar img').each(function() {
+			prc_img($(this), '');
+		});
+	}
+
+	function theme_main($) {
+		$('.manage-option img').each(function() {
+			prc_img($(this), 'main', 'icon');
+		});
+		$('#main-panel img').each(function() {
+			prc_img($(this), 'main');
+		});
+		$('#main-panel').on('DOMSubtreeModified', () => {
+			let imgs = $('#main-panel img');
+			if (imgs.length) {
+				prc_img($(imgs[0]), 'main');
+			}
+		})
+	}
+
+	function prc_img(img, pos, clazz) {
+		let pt = img.parent();
+
+		let src = img.attr('src');
+		let file = src.substr(src.lastIndexOf('/') + 1);
+
+
+		let icon = mapping(file, pos, pt.attr('href'));
+		let sz = img.height();
+		if (icon && icon != 'SKIP') {
+			img.replaceWith(`<span class="mdi mdi-${sz}px ${icon} ${clazz||''}"></span>`);
+		}
+	}
+
+	document.addEventListener("DOMContentLoaded", function() {
+		// load jquery
+		webpackJsonp[0][1][0](window);
+
+		// process conflict
+		window.jQuery.noConflict();
+
+		init_theme(jQuery);
+	});
+})();
